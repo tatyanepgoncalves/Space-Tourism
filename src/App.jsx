db62a01088782cdc2c3ebbf18da60430f473f28b
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-12-06 11:07:02 -0300

    :sparkles: feat: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import Header from "./components/Header/Header";
+import "./styles/global.scss";
+
 export default function App() {
-  return <>App</>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
