dd65b123507f8800ec832254514da7868802cff5
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-12-09 10:34:00 -0300

    :wastebasket: remove: Removendo arquivos não utilizados do projeto

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,7 @@ import "../../styles/Footer.scss";
 export default function Footer() {
   return (
     <footer>
-      <div className="container">
-        <p>Desafio do <a href="https://www.frontendmentor.io" target="_blank">Frontend Mentor</a>. Desenvolvido por <a href="https://github.com/tatyanepgoncalves" target="_blank">Tatyane Gonçalves</a>.</p>
-      </div>
+      <p>Desafio do <a href="https://www.frontendmentor.io" target="_blank">Frontend Mentor</a>. Desenvolvido por <a href="https://github.com/tatyanepgoncalves" target="_blank">Tatyane Gonçalves</a>.</p>
     </footer>
   )
 }
